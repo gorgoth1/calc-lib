540de81fbb6e3845035547a40746af897d166639
Grzegorz <Grzegorz@grzegorz.pl>
2016-12-14 13:03:38 +0100

    Travis

--- src/test/java/pl/gov/coi/example/calc/CalcTest.java
+++ src/test/java/pl/gov/coi/example/calc/CalcTest.java
@@ -24,6 +24,48 @@ public class CalcTest {
     }
 
     @Test
+    public void testAdd1() {
+        // given
+        int a = 6;
+        int b = 1;
+        Calc calc = new Calc();
+
+        // when
+        int result = calc.add(a, b);
+
+        // then
+        assertEquals(7, result);
+    }
+
+    @Test
+    public void testAdd2() {
+        // given
+        int a = 4;
+        int b = 4;
+        Calc calc = new Calc();
+
+        // when
+        int result = calc.add(a, b);
+
+        // then
+        assertEquals(8, result);
+    }
+
+    @Test
+    public void testAdd3() {
+        // given
+        int a = 4;
+        int b = 1;
+        Calc calc = new Calc();
+
+        // when
+        int result = calc.add(a, b);
+
+        // then
+        assertEquals(5, result);
+    }
+
+    @Test
     public void testSub() {
         // given
         int a = 5;
